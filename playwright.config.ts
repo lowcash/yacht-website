3eeedc6138cea7e400de42d5b616cfce9f246995
lukas <machala.lukas@post.cz>
2026-04-08 23:36:11 +0700

    build(yacht): update project configs for lighthouse testing
    - Add lighthouse config to .gitignore
    - Update playwright.config.ts for performance measurement
    - Adjust tsconfig.app.json

--- playwright.config.ts
+++ playwright.config.ts
@@ -21,10 +21,12 @@ export default defineConfig({
     {
       name: 'desktop-chrome',
       use: { ...devices['Desktop Chrome'] },
+      testIgnore: ['**/*.mobile.spec.ts'],
     },
     {
       name: 'mobile-safari',
       use: { ...devices['iPhone 14'] },
+      testIgnore: ['**/*.desktop.spec.ts'],
     },
   ],
 })
